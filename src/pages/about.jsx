41d5e3d47f073d0514e9edd7690f81b0f281636d
mia2380 <112995643+mia2380@users.noreply.github.com>
2022-11-25 17:41:43 -0500

    Add files via upload

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div className="about">
+            <h4>By: Brenda Allemand</h4>
+            <h4>email:balle@gmail.com</h4>
+        </div>
+    );
+};
+
+export default About;
